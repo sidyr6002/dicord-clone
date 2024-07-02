beedc1c0bb4c83150619f1c11680f0b0060f12d9
sidyr6002 <siddhardh6002@gmail.com>
2024-07-02 16:05:58 +0530

    bug fix

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -37,7 +37,7 @@ export const useChatQuery = ({ apiURL, queryKey, paramKey, paramValue }: ChatQue
         queryFn: fetchMessages,
         initialPageParam: undefined,
         getNextPageParam: (lastPage, pages) => lastPage?.nextCursor,
-        refetchInterval: isConnected ? false : 1000
+        refetchInterval: 1000
     })
 
     //console.log("data", data);
